1c96f693fd967f60078c647e2d9a58464fef75e5
Vadim Smagin <magenvadim@gmail.com>
2024-01-04 03:33:30 +0200

    #1 first commit

---
+++ src/Components/Aside.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Aside() {
+  return (
+    <div className='aside'>
+      <div className="logo">
+        <a href="#"><span>A</span>tlas</a>
+      </div>
+      <ul className="nav">
+            <li><a href="#" className="active"><i className="fa fa-home"></i>Home</a></li>
+            <li><a href="#"><i className="fa fa-user"></i>About</a></li>
+            <li><a href="#"><i className="fa fa-list"></i>Services</a></li>
+            <li><a href="#"><i className="fa fa-briefcase"></i>Portfolio</a></li>
+            <li><a href="#"><i className="fa fa-comments"></i>Contact</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Aside
---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='main-content'>
+        <section className="home section">
+            Home
+        </section>      
+    </div>
+  )
+}
+
+export default Home
