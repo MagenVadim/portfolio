4a882e95635f665c2d583e6789bb1d01ffd36578
Vadim Smagin <magenvadim@gmail.com>
2024-01-24 00:24:53 +0200

    #27 Last projects was added

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -4,11 +4,15 @@ import {Section} from '../StylesComponent'
 import countries from '../../images/countriesAPI.jpg'
 import jobListing from '../../images/JobListing.png'
 import snickersStore from '../../images/snickersStore.jpg'
+import notification from '../../images/notification.png'
+import todo from '../../images/todo.png'
 
 const linkSourse = [
     {name: "jobListing", url: "https://main.dfuhavulmhomn.amplifyapp.com/"},
     {name: "countriesAPI", url: "https://main.d1x8la6yj0iyv6.amplifyapp.com/"},
-    {name: "snickersStore", url: "https://main.d3lm5srdjp1fwh.amplifyapp.com/"}
+    {name: "snickersStore", url: "https://main.d3lm5srdjp1fwh.amplifyapp.com/"},
+    {name: "notification", url: "https://main.d4uo3va94tu7d.amplifyapp.com/"},
+    {name: "todo", url: "https://main.d1t4lt62kkmhda.amplifyapp.com/"}
 ]
 
 
@@ -31,9 +35,8 @@ function Portfolio({navToglerValue, lightDarkMode}) {
                     <PortfolioItem key={"p1"} imgSource={jobListing} url={linkSourse[0].url}/>
                     <PortfolioItem key={"p2"} imgSource={countries} url={linkSourse[1].url}/>
                     <PortfolioItem key={"p3"} imgSource={snickersStore} url={linkSourse[2].url}/>
-                    <PortfolioItem key={"p4"} imgSource={jobListing}/>
-                    <PortfolioItem key={"p5"} imgSource={jobListing}/>
-                    <PortfolioItem key={"p6"} imgSource={jobListing}/>
+                    <PortfolioItem key={"p4"} imgSource={notification} url={linkSourse[3].url}/> 
+                    <PortfolioItem key={"p5"} imgSource={todo} url={linkSourse[4].url}/>                    
                 </div>
 
             </div>
